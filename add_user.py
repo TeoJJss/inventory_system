89af2c868d224b270dd08be3ec6358b678321512
TeoJJss <teojunjia@gmail.com>
2023-04-21 16:17:30 +0800

    file_dir & Assumption

--- add_user.py
+++ add_user.py
@@ -2,80 +2,87 @@
 Script for ADD USER function
 """
 
-# Initialization
-role="admin" # Get from user_authentication function
-user_info=""
-access_allowed=("admin",) # Only admin can add new user
+def add_user(role): # role get from user_auth
+    # Assume user's password must be minimum 8 in length
+    # Assume there are only 3 roles that will use the system, which are "admin", "inventory-checker" and "purchaser" 
+    # Assume that "userdata.txt" is placed in the same directory as this file
 
-# Validate user type
-if role not in access_allowed:
-    print("REJECTED: You have no permission to add user, please login again!")
+    # Initialization
+    file_dir="userdata.txt"
+    user_info=""
+    access_allowed=("admin",) # Only admin can add new user
 
-# If user is admin
-else:
-    user_type_allowed=("admin", "inventory-checker", "purchaser") # 3 types of user allowed to input
+    # Validate user type
+    if role not in access_allowed:
+        print("REJECTED: You have no permission to add user, please login again!")
 
-    # Repeat asking user to input new user's details until request to exit
-    while True:
-        # Welcome message
-        print("You are now at: ▶ Add New User (Admin only) ◀\nTo add a new user, you need to specify username, password and user type. Please type these data separated by comma(,)\nMinimum 8 characters required for password.\nWARNING: username and password are both case-sensitive and space-sensitive!\n\n")
-        #Guide user to input
-        print("\nFormat: <username>,<password>,<user_type>\nExample input: Ali,1234A@bc,purchaser\nTo return back to main menu, type \"q\"\n")
+    # If user is admin
+    else:
+        user_type_allowed=("admin", "inventory-checker", "purchaser") # 3 types of user allowed to input
 
-        # Get new user's info 
-        user_info=(input("Please enter new user's info: ").rstrip()).split(",")
+        # Repeat asking user to input new user's details until request to exit
+        while True:
+            # Welcome message
+            print("You are now at: ▶ Add New User (Admin only) ◀\nTo add a new user, you need to specify username, password and user type. Please type these data separated by comma(,)\nMinimum 8 characters required for password.\nWARNING: username and password are both case-sensitive and space-sensitive!\n\n")
+            #Guide user to input
+            print("\nFormat: <username>,<password>,<user_type>\nExample input: Ali,1234A@bc,purchaser\nTo return back to main menu, type \"q\"\n")
 
-        try:
-            # Data validation
-            # If user choose to exit
-            if len(user_info)==1 and "q" in user_info:
-                break
+            # Get new user's info 
+            user_info=(input("Please enter new user's info: ").rstrip()).split(",")
 
-            # If not 2 commas in the input
-            if len(user_info) !=3:
-                raise Exception("Please input username, password and user type! ")
-            
-            # If username is empty
-            if len(user_info[0].strip())<1:
-                raise Exception("Username cannot be empty! ")
-            
-            # If password length not exceed 8
-            if len(user_info[1])<8 :
-                raise Exception("Password length must more than or equal to 8!")
-            
-            # If user type is invalid
-            if user_info[2].lower().strip() not in user_type_allowed:
-                raise Exception("Invalid user type!")
-            
-            # After data validation pass
-            # Confirm with user before adding the data into txt
-            username, password, user_type=user_info
-            print(f"\n\nNew user info\nusername: {username.strip()}\t|\tpassword: {password}\t|\tuser_type: {user_type.lower().strip()}")
-            print("\nType 'y' to confirm, or any characters to discard")
-
-            # If user confirmed, add data
-            if input("Confirm? [y]: ").lower().strip() == "y":
-                row_num=sum(1 for x in open("userdata.txt", "r")) # Get row number
-
-                # Open file and append userdata
-                with open("userdata.txt", "a") as credential_file:
-                    credential_file.write(f"{row_num+1}\t{username.strip()}\t{password}\t{user_type.lower().strip()}\n")
-                    print("\nAdded successfully!\n\n")
-
-                # Ask user if want to add new user or exit
-                print("Type 'y' to add another user, other characters to quit. ")
-                if input("Add another user [y] or exit?").lower().strip() =="y": # If user request to continue adding new user
-                    continue
-                else: # If user request to exit
+            try:
+                # Data validation
+                # If user choose to exit
+                if len(user_info)==1 and "q" in user_info:
                     break
-            
-            # If user choose to discard data
-            else:
-                print("\nDISCARDED\n")
-                continue # Set user_info as empty string so it can be repeated instead of quit
 
-        # Error handler
-        except Exception as e:
-            user_info="" # user info reset if any error is found
-            print("\n\nERROR:",e, "\n")
-print("\nEXIT add user function\n")   
+                # If not 2 commas in the input
+                if len(user_info) !=3:
+                    raise Exception("Please input username, password and user type! ")
+                
+                # If username is empty
+                if len(user_info[0].strip())<1:
+                    raise Exception("Username cannot be empty! ")
+                
+                # If password length not exceed 8
+                if len(user_info[1])<8 :
+                    raise Exception("Password length must more than or equal to 8!")
+                
+                # If user type is invalid
+                if user_info[2].lower().strip() not in user_type_allowed:
+                    raise Exception("Invalid user type!")
+                
+                # After data validation pass
+                # Confirm with user before adding the data into txt
+                username, password, user_type=user_info
+                print(f"\n\nNew user info\nusername: {username.strip()}\t|\tpassword: {password}\t|\tuser_type: {user_type.lower().strip()}")
+                print("\nType 'y' to confirm, or any characters to discard")
+
+                # If user confirmed, add data
+                if input("Confirm? [y]: ").lower().strip() == "y":
+                    row_num=sum(1 for x in open(file_dir, "r")) # Get row number
+
+                    # Open file and append userdata
+                    with open(file_dir, "a") as credential_file:
+                        credential_file.write(f"{row_num+1}\t{username.strip()}\t{password}\t{user_type.lower().strip()}\n")
+                        print("\nAdded successfully!\n\n")
+
+                    # Ask user if want to add new user or exit
+                    print("Type 'y' to add another user, other characters to quit. ")
+                    if input("Add another user [y] or exit?").lower().strip() =="y": # If user request to continue adding new user
+                        continue
+                    else: # If user request to exit
+                        break
+                
+                # If user choose to discard data
+                else:
+                    print("\nDISCARDED\n")
+                    continue # Set user_info as empty string so it can be repeated instead of quit
+
+            # Error handler
+            except Exception as e:
+                user_info="" # user info reset if any error is found
+                print("\n\nERROR:",e, "\n")
+    print("\nEXIT add user function\n")   
+
+add_user(role="admin")
--- update_item.py
+++ update_item.py
@@ -2,155 +2,164 @@
 Script to update items
 """
 
-# Initialization
-role="admin" # Get from user_authentication function
-access_allowed=("admin",) # Only admin can add new user
-code=change=txt=""
-d={
-    "1": "Code",
-    "2": "Description",
-    "3": "Category",
-    "4": "Unit",
-    "5": "Price", 
-    "6": "Quantity",
-    "7": "Minimum (Threshold)"
-}
+def update_item(role):
+    # Assume item code's format is 5-digit numbers and unique
+    # Assume that "inventory.txt" is placed in the same directory as this file
+    # Assume that price must be in 2 decimal format
+    # Assume that the data entered accurately reflects the actual situation
+    # Validation on the format of the data only, not the reliability
 
-# This will be repeated until user requests to exit
-while True:
-    data_ls=[]
-    code_ls=[]
-    #check if user is admin
-    if role not in access_allowed:
-        print("REJECTED: You have no permission to update items, please login again!")
-        break
-    else:
-        print("You are now at: ▶ Update Items (Admin only) ◀\n")
-        # Get all records out from txt file
-        with open("inventory.txt", "r") as inventory_file:
-            # Display records from txt file in a neat format
-            print("{:10} {:20} {:15} {:10} {:10} {:10} {:10}".format(*["Code", "Description", "Category", "Unit", "Price", "Quantity", "Minimum (Threshold)"]))
-            print("---"*35)
-            for line in inventory_file:
-                print("{:10} {:20} {:15} {:10} {:10}\t{:10}\t{:10}".format(*(line.strip().split("\t")))) # Specify field size for each column
-            
-            # Save each line into a list
-            inventory_file.seek(0)
-            data_ls=inventory_file.readlines()
+    # Initialization
+    file_dir="inventory.txt" # Locate the txt file directory
+    access_allowed=("admin",) # Only admin can add new user
+    code=change=""
+    d={
+        "1": "Code",
+        "2": "Description",
+        "3": "Category",
+        "4": "Unit",
+        "5": "Price", 
+        "6": "Quantity",
+        "7": "Minimum (Threshold)"
+    }
 
-        # Convert to 2D list
-        for ind, data in enumerate(data_ls):
-            data_ls[ind]=data.rstrip().split("\t")
-        # Save item code into a new list
-        for line in data_ls:
-            code_ls.append(line[0])
+    # This will be repeated until user requests to exit
+    while True:
+        data_ls=[]
+        code_ls=[]
+        #check if user is admin
+        if role not in access_allowed:
+            print("REJECTED: You have no permission to update items, please login again!")
+            break
+        else:
+            print("You are now at: ▶ Update Items (Admin only) ◀\n")
+            # Get all records out from txt file
+            with open(file_dir, "r") as inventory_file:
+                # Display records from txt file in a neat format
+                print("{:10} {:20} {:15} {:10} {:10} {:10} {:10}".format(*["Code", "Description", "Category", "Unit", "Price", "Quantity", "Minimum (Threshold)"]))
+                print("---"*35)
+                for line in inventory_file:
+                    print("{:10} {:20} {:15} {:10} {:10}\t{:10}\t{:10}".format(*(line.strip().split("\t")))) # Specify field size for each column
+                
+                # Save each line into a list
+                inventory_file.seek(0)
+                data_ls=inventory_file.readlines()
 
-        try:
-            print("\nAbove are the existing records\n\ntype 'q' to return back to main menu. ")
+            # Convert to 2D list
+            for ind, data in enumerate(data_ls):
+                data_ls[ind]=data.rstrip().split("\t")
+            # Save item code into a new list
+            for line in data_ls:
+                code_ls.append(line[0])
 
-            #Request to input item code only when it's empty
-            if not code:
-                code=input("Specify the item's code which you would like to update? ").strip()
+            try:
+                print("\nAbove are the existing records\n\ntype 'q' to return back to main menu. ")
 
-            # If user choose to quit
-            if code=="q":
-                break
-            # Check if the input item code exist
-            if code not in code_ls:
-                print(code)
-                code=""
-                raise Exception("The item code is not found! ")
+                #Request to input item code only when it's empty
+                if not code:
+                    code=input("Specify the item's code which you would like to update? ").strip()
 
-            # Request user to choose what to update for the item code's row
-            print("""\nWhat do you wish to update?
-            Type 1  Item code (MUST be 5 digits)
-            Type 2  Description
-            Type 3  Category
-            Type 4  Unit
-            Type 5  Price
-            Type 6  Quantity
-            Type 7  Minimum (Threshold)
-
-            Type b to go back item list
-            Type q to go back main menu\n
-            """)
-            print(f"\nYou're now updating: {code}")
-            change=input("Your option: ").strip()
-
-            # Check if user want to quit or go back to main menu
-            if not change.isnumeric():
-                if change=="b":
-                    code="" # Reset item code to empty
-                    print("\nBACK to item list\n")
-                    continue
-                elif change=="q":
+                # If user choose to quit
+                if code=="q":
                     break
-                else:
-                    raise Exception("Invalid Option!\n")
+                # Check if the input item code exist
+                if code not in code_ls:
+                    print(code)
+                    code=""
+                    raise Exception("The item code is not found! ")
 
-            # Check if option is valid
-            elif int(change) not in range(1,8):
-                raise Exception("Invalid option!\n")
-            
-            # Locate the element to change
-            row=code_ls.index(code)
-            column=int(change)
+                # Request user to choose what to update for the item code's row
+                print("""\nWhat do you wish to update?
+                Type 1  Item code (MUST be 5 digits)
+                Type 2  Description
+                Type 3  Category
+                Type 4  Unit
+                Type 5  Price
+                Type 6  Quantity
+                Type 7  Minimum (Threshold)
 
-            print(f"\n\nYou're now changing the item code: {code}, for its {d[change]}")
-            print("[Enter 'b' to go back item list]")
-            new_value=input("New value: ").strip()
-            
-            if new_value != "b":
-                # Data validation
-                if change=="1": # If request to change item code
-                    if new_value in code_ls: # Code must be unique
-                        raise Exception("Item code must be unique!\nThe new Code is either the same with the previous, or conflicts with other items' code.\n")
-                    elif len(new_value) != 5 or not new_value.isnumeric(): # Code must contain 5 digits
-                        raise Exception("Item code must be 5 digits!\n")
-                    code=new_value
-                elif change in ["2", "3", "4"]: # If request to change description/category/unit
-                    if not new_value.strip(): # Description cannot be changed to empty
-                        raise Exception(f"{d[change]} cannot be empty!\n")
-                elif change in ["5", "6", "7"]: # If request to change Price/Quantity/Minimum
-                    # Price/Quantity/Minimum must be number
-                    try:
-                        float(new_value)
-                    except:
-                        raise Exception(f"{d[change]} must be a number! \n")
-                    
-                    # Price will be converted to 2 decimal automatically
-                    if change=="5":
-                        new_value="%.2f" % float(new_value)
-                    # Quantity and Minimum must be integer
+                Type b to go back item list
+                Type q to go back main menu\n
+                """)
+                print(f"\nYou're now updating: {code}")
+                change=input("Your option: ").strip()
+
+                # Check if user want to quit or go back to main menu
+                if not change.isnumeric():
+                    if change=="b":
+                        code="" # Reset item code to empty
+                        print("\nBACK to item list\n")
+                        continue
+                    elif change=="q":
+                        break
                     else:
+                        raise Exception("Invalid Option!\n")
+
+                # Check if option is valid
+                elif int(change) not in range(1,8):
+                    raise Exception("Invalid option!\n")
+                
+                # Locate the element to change
+                row=code_ls.index(code)
+                column=int(change)
+
+                print(f"\n\nYou're now changing the item code: {code}, for its {d[change]}")
+                print("[Enter 'b' to go back item list]")
+                new_value=input("New value: ").strip()
+                
+                if new_value != "b":
+                    # Data validation
+                    if change=="1": # If request to change item code
+                        if new_value in code_ls: # Code must be unique
+                            raise Exception("Item code must be unique!\nThe new Code is either the same with the previous, or conflicts with other items' code.\n")
+                        elif len(new_value) != 5 or not new_value.isnumeric(): # Code must contain 5 digits
+                            raise Exception("Item code must be 5 digits!\n")
+                        code=new_value
+                    elif change in ["2", "3", "4"]: # If request to change description/category/unit
+                        if not new_value.strip(): # Description cannot be changed to empty
+                            raise Exception(f"{d[change]} cannot be empty!\n")
+                    elif change in ["5", "6", "7"]: # If request to change Price/Quantity/Minimum
+                        # Price/Quantity/Minimum must be number
                         try:
-                            new_value=int(new_value)
-                        except: # Error if user input decimal
-                            raise Exception(f"{d[change]} must be integer! \n")
+                            float(new_value)
+                        except:
+                            raise Exception(f"{d[change]} must be a number! \n")
+                        
+                        # Price will be converted to 2 decimal automatically
+                        if change=="5":
+                            new_value="%.2f" % float(new_value)
+                        # Quantity and Minimum must be integer
+                        else:
+                            try:
+                                new_value=int(new_value)
+                            except: # Error if user input decimal
+                                raise Exception(f"{d[change]} must be integer! \n")
 
+                        
+                    # If pass all check, change the value in the list        
+                    data_ls[row][column-1]=str(new_value)
                     
-                # If pass all check, change the value in the list        
-                data_ls[row][column-1]=str(new_value)
-                
-                # Update the changes with the list
-                with open("inventory.txt", "w") as inventory_file:
-                    for row in data_ls:
-                        if data_ls.index(row) != 0: # After writing each row, break line
-                            inventory_file.write("\n")
-                        for column in row: # Use TAB to separate columns
-                            inventory_file.write(str(column)+"\t")
-                
-                # Ask if user want to continue updating the same item code or new item code
-                if input(f"Type 'y' to continue updating other details for {code},\nor any other characters to switch: ").strip().lower()=='y':
-                    continue
+                    # Update the changes with the list
+                    with open(file_dir, "w") as inventory_file:
+                        for row in data_ls:
+                            if data_ls.index(row) != 0: # After writing each row, break line
+                                inventory_file.write("\n")
+                            for column in row: # Use TAB to separate columns
+                                inventory_file.write(str(column)+"\t")
+                    
+                    # Ask if user want to continue updating the same item code or new item code
+                    if input(f"Type 'y' to continue updating other details for {code},\nor any other characters to switch: ").strip().lower()=='y':
+                        continue
+                    else:
+                        code="" # Reset item code to empty
+                        continue
                 else:
-                    code="" # Reset item code to empty
-                    continue
-            else:
-                print("\nBACK to item list\n")
+                    print("\nBACK to item list\n")
 
-        # Error handler
-        except Exception as e:
-            print("\n\nERROR:",e, "\n") # Display error message
-            continue
-print("\nEXIT update item function")
+            # Error handler
+            except Exception as e:
+                print("\n\nERROR:",e, "\n") # Display error message
+                continue
+    print("\nEXIT update item function")
+
+update_item(role="admin")
